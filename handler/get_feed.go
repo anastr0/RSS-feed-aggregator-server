2c247fa96c90b44a6a1048e4e197c47c4980d628
Anaswara T Rajan <anaswaratrajan@gmail.com>
2021-03-16 23:22:38 +0530

    All models required

--- handler/get_feed.go
+++ handler/get_feed.go
@@ -13,6 +13,50 @@ import (
 	"sort"
 )
 
+// models
+
+type reqFeedURL struct {
+	feedURL string `json:"feedURL"`
+}
+
+type RSS struct {
+	Channel Channel `xml:"channel"`
+}
+
+type Channel struct {
+	Title string `xml:"title"`
+	Link  string `xml:"link"`
+	Desc  string `xml:"description"`
+	Items []Item `xml:"item"`
+}
+
+type Item struct {
+	Title     string    `xml:"title"`
+	Link      string    `xml:"link"`
+	Desc      string    `xml:"description"`
+	Guid      string    `xml:"guid"`
+	Source   string    `xml:"source"`
+	Author    string    `xml:"author"`
+   PubDate    string `xml:"pubDate"`
+ }
+
+type RSSFeedItem struct {
+	PubDate    time.Time `json:"pubDate"`
+	Title     string    `json:"title"`
+	Link      string   `json:"link"`
+	Desc      string   `json:"description"`
+	Guid      string   `json:"guid,omitempty"`
+	Source   string   `json:"source,omitempty"`
+	Author    string   `json:"author,omitempty"`
+	
+}
+
+type RSSFeed struct {
+	Updated			time.Time //`json:"LastUpdated"`
+	RSSFeedList []RSSFeedItem //`json:"RSSFeedList"`
+}
+
+
 func RSSFeedHandler(c echo.Context) error {
   // Take feedURL as formData
   // TODO: Get RSS feed and return to client
