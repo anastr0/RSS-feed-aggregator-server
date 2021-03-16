2a4356460f95264d4f1cdfbc43e030f8bb06d88f
Anaswara T Rajan <anaswaratrajan@gmail.com>
2021-03-16 23:23:50 +0530

    Task 1 - get RSS feed to given feedURL

--- handler/get_feed.go
+++ handler/get_feed.go
@@ -57,11 +57,63 @@ type RSSFeed struct {
 }
 
 
-func RSSFeedHandler(c echo.Context) error {
-  // Take feedURL as formData
-  // TODO: Get RSS feed and return to client
+func fetchRSS(feedURL string) []RSSFeedItem {
 
-  feedURL := c.FormValue("feedURL") 
-  return c.String(http.StatusOK, feedURL) 
+	// fetch content from given feedURL and decodes into rss struct, returns list of items
+	resp, err := http.Get(feedURL) // fetch from feedURL
+	if err != nil {
+		log.Fatalln(err)
+	}
+	rss := RSS{} // new RSS model
+	reqBody, err := ioutil.ReadAll(resp.Body)
+    xml.Unmarshal(reqBody, &rss)
 
+	RSSFeed := make([]RSSFeedItem, 0)
+
+	// return feed items list
+	for _, item := range rss.Channel.Items {
+		pubDate := parseDate(item.PubDate) // parse date from dateString
+		rssFeedItem := RSSFeedItem{ 
+			PubDate:        pubDate,
+			Title:     item.Title,   
+			Link:      item.Link,
+			Desc:      item.Desc, 
+			Guid:      item.Guid,
+			Source:  item.Source, 
+			Author:   item.Author,  
+		}
+		RSSFeed = append(RSSFeed, rssFeedItem)
+	}
+	return RSSFeed 
 }
+
+func getRSSFeed(feedURL string) []RSSFeedItem {
+	// called by getRSSFeedhandler
+	// returns list of feed items from given feedURL
+
+	rssFeed := fetchRSS(feedURL) // fetch and get feeditems
+	// sort RSS feed items
+	sort.Slice(rssFeed, func(i, j int) bool {
+		return rssFeed[j].PubDate.Before(rssFeed[i].PubDate)
+	})
+	return rssFeed
+}
+func RSSFeedHandler(c echo.Context) error {
+	// : Get RSS feed to given feedURL and return to client
+
+	// Read json from request body and get feedURL
+	json_map := make(map[string]interface{}) 
+	err := json.NewDecoder(c.Request().Body).Decode(&json_map)
+	feedURL := json_map["feedURL"].(string)
+	if err!=nil {
+		return c.String(http.StatusOK, "Provide valid Json")
+	}
+
+	// Get RSS feed for the given feedURL and return as JSON
+	rssFeed := &RSSFeed{
+		Updated: time.Now(),
+		RSSFeedList: getRSSFeed(feedURL),
+	}
+	return c.JSON(http.StatusOK, rssFeed)
+}
+
